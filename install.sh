67c7040fa6ba456998e90b286db5e5e074c7d4de
zhangxin <zhangxin8069@qq.com>
2023-07-29 21:01:59 +0800

    2023-07-29-21-01-59

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ pushd ${install_path}
 tar xzf ${file_path}/openmpi-4.1.5.tar.gz && pushd openmpi-4.1.5
 ./configure --prefix=${MPI_HOME}
 make -j16
-make install .
+make install
 pushd examples
 mpicxx hello_cxx.cc
 mpirun -np 4 a.out
